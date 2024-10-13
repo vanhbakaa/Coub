f8472a6442b9225f789e2d884d4b9413d2750f13
Vanhbaka <vanhbaka2k7@gmail.com>
2024-10-13 09:53:45 +0700

    Fixed bugs and add all tasks

--- bot/core/tasks.py
+++ bot/core/tasks.py
@@ -1,28 +1,27 @@
 tasks = {
   1: {
     "id": 1,
-    "type": "ref",
-    "title": "tasks.ref",
-    "price": 5,
-    "icon": "ref",
-    "status": "completed",
+    "type": "welcome",
+    "title": "welcome",
+    "price": 10,
+    "icon": "welcome",
     "repeatable": False,
-    "rewardCreatedAt": "2024-10-11T03:08:59.207Z"
+    "status": "ready-to-claim"
   },
   2: {
     "id": 2,
     "type": "checkin",
-    "title": "tasks.dailyCheckIn",
+    "title": "daily Check-In",
     "price": 5,
     "icon": "checkin",
     "status": "completed",
     "repeatable": True,
-    "rewardCreatedAt": "2024-10-11T03:08:59.207Z"
+    "rewardCreatedAt": "2024-10-12T13:32:18.179Z"
   },
   3: {
     "id": 3,
     "type": "watch",
-    "title": "tasks.watch10Coub",
+    "title": "watch 10 Coub",
     "price": 20,
     "icon": "video-octagon",
     "repeatable": False,
@@ -31,7 +30,7 @@ tasks = {
   4: {
     "id": 4,
     "type": "share",
-    "title": "tasks.share3Coub",
+    "title": "share 3 Coub",
     "price": 15,
     "icon": "share",
     "repeatable": False,
@@ -40,17 +39,16 @@ tasks = {
   6: {
     "id": 6,
     "type": "twitter",
-    "title": "tasks.followX",
+    "title": "follow X",
     "price": 10,
     "icon": "twitter",
     "repeatable": False,
     "status": "ready-to-start"
-
   },
   7: {
     "id": 7,
     "type": "telegram",
-    "title": "tasks.followTg",
+    "title": "follow Tg",
     "price": 10,
     "icon": "telegram",
     "repeatable": False,
@@ -59,25 +57,43 @@ tasks = {
   9: {
     "id": 9,
     "type": "youtube",
-    "title": "tasks.followYoutube",
+    "title": "follow Youtube",
     "price": 10,
     "icon": "youtube",
     "repeatable": False,
     "status": "ready-to-start"
   },
+  8: {
+    "id": 8,
+    "type": "onboarding",
+    "title": "completed Tasks",
+    "price": 100,
+    "icon": "onboarding",
+    "repeatable": False,
+    "status": "ready-to-start"
+  },
   12: {
     "id": 12,
-    "type": "watch",
-    "title": "tasks.coubsInRandom25",
+    "type": "watch-25-random",
+    "title": "Watch coubs In Random 25",
     "price": 10,
     "icon": "watch",
-    "status": "ready-to-start",
+    "status": "started",
     "repeatable": True
   },
   13: {
     "id": 13,
-    "type": "watch",
-    "title": "tasks.coubsInRising25",
+    "type": "watch-25-rising",
+    "title": "Watch coubs In Rising 25",
+    "price": 10,
+    "icon": "watch",
+    "status": "started",
+    "repeatable": True
+  },
+  14: {
+    "id": 14,
+    "type": "watch-25-recommendations",
+    "title": "Watch coubs In For You 25",
     "price": 10,
     "icon": "watch",
     "status": "ready-to-start",
@@ -85,8 +101,8 @@ tasks = {
   },
   15: {
     "id": 15,
-    "type": "like",
-    "title": "random",
+    "type": "like-5-random",
+    "title": "Like in Random 5",
     "price": 5,
     "icon": "like",
     "status": "ready-to-start",
@@ -94,8 +110,17 @@ tasks = {
   },
   16: {
     "id": 16,
-    "type": "like",
-    "title": "rising",
+    "type": "like-5-rising",
+    "title": "like in Rising 5",
+    "price": 5,
+    "icon": "like",
+    "status": "started",
+    "repeatable": True
+  },
+  17: {
+    "id": 17,
+    "type": "like-5-recommendations",
+    "title": "like in For You 5",
     "price": 5,
     "icon": "like",
     "status": "ready-to-start",
@@ -104,22 +129,40 @@ tasks = {
   18: {
     "id": 18,
     "type": "share-wtf-video",
-    "title": "tasks.shareWTFStory",
+    "title": "share WTF Story",
     "price": 30,
     "icon": "share-coub",
-    "repeatable": False,
-    "status": "ready-to-start"
+    "status": "ready-to-start",
+    "repeatable": False
   },
   19: {
     "id": 19,
-    "type": "share",
-    "title": "tasks.shareStory",
+    "type": "share-story",
+    "title": "share Story",
     "price": 5,
     "icon": "share-story",
     "status": "ready-to-start",
     "repeatable": True
   },
-  32: {
+  25: {
+    "id": 25,
+    "type": "edit-channel",
+    "title": "edit Channel",
+    "price": 5,
+    "icon": "coub",
+    "status": "ready-to-start",
+    "repeatable": False
+  },
+  26: {
+    "id": 26,
+    "type": "like-5-sports",
+    "title": "like Sports",
+    "price": 5,
+    "icon": "like",
+    "status": "ready-to-start",
+    "repeatable": True
+  },
+ 32: {
     "id": 32,
     "type": "coub-app-trending",
     "title": "#CoubTrending Like & RT",
